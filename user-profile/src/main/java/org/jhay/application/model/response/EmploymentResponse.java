2413435b8ec1203344f66640e32ec6b1cf6064c9
Mhister-Jhay <omosighojosh@gmail.com>
2023-06-15 15:03:32 +0100

    Added Caching feature for Employment record and User address using Redis

--- user-profile/src/main/java/org/jhay/application/model/response/EmploymentResponse.java
+++ user-profile/src/main/java/org/jhay/application/model/response/EmploymentResponse.java
@@ -5,11 +5,13 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
+
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-public class EmploymentResponse {
+public class EmploymentResponse implements Serializable {
     private Long id;
     private String employmentStatus;
     private String position;
